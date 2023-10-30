611753f1ce712fb2f56db948309c3af2093e185e
Bonnie Chen <114087522+Bonnie0819@users.noreply.github.com>
2023-10-29 20:19:51 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,75 @@
-function initialize() {
-    score = 0;  //score of user
-    flipped = 0; //how many images are flipped over
-    const image1 = new Image();
-    img2 = "";
-    img3 = "";
-    img4 = "";
-    img5 = "";
-    img6 = "";
-    img7 = "";
-    img8 = "";
-    img9 = "";
-    img10 = "";
-    img11 = "";
-    img12 = "";
-    img13 = "";
-    img14 = "";
-    img15 = "";
-    img16 = "";
+score = 0;
+flippedOver = 0;    //how many cards are flipped
+square1 = 0;
+square2 = 0;
 
-    hiddenImages();
-    
+
+function flipSquare(id) {
+    //changes the image of the squares
+    if(id == 1 || id == 9) {
+        document.getElementById(id).src = "panda1.jpeg";
+    }
+    if(id == 2 || id == 10) {
+        document.getElementById(id).src = "panda2.jpeg";
+    }
+    if(id == 3 || id == 11) {
+        document.getElementById(id).src = "panda3.jpeg";
+    }
+    if(id == 4 || id == 12) {
+        document.getElementById(id).src = "panda4.jpeg";
+    }
+    if(id == 5 || id == 13) {
+        document.getElementById(id).src = "grabbedrat.jpeg";
+    }
+    if(id == 6 || id == 14) {
+        document.getElementById(id).src = "staringrat.jpeg";
+    }
+    if(id == 7 || id == 15) {
+        document.getElementById(id).src = "fluffyrat.gif.MOV";
+    }
+    if(id == 8 || id == 16) {
+        document.getElementById(id).src = "torpedorat.gif.MOV";
+    }
+
+    if(flippedOver == 0) {
+        if(square1 !== id) {   
+            square1 = id;
+            flippedOver++;
+        }
+    } else if(flippedOver == 1) {
+        square2 = id;
+        flippedOver++;
+        matchSquares(square1, square2);
+    }
 }
+
+//checks to see if the squares match
+function matchSquares() {
+    flippedOver = 0;    //sets the flipped squares to 0
+    score++;
+    document.getElementById("score").innerHTML = "Score: " + score;
+    if(document.getElementById(square1).src == document.getElementById(square2).src) {
+        return
+    } else {        //sets the image back to the question mark
+        document.getElementById(square1).src = "question.png";
+        document.getElementById(square2).src = "question.png";
+        square1 = square2 = "";
+    }
+}
+
+//resets the game
+function reset() {
+    score = 0;
+    flippedOver = 0;
+    square1 = square2 = "";
+    document.getElementById("score").innerHTML = "Score: " + score;
+    //resets all grid images to question marks
+    for(let i = 1; i <= 16; i++) {
+        document.getElementById(i).src = "question.png";
+    }
+}
+
+
 
 //shuffles the images on the grid
 function hiddenImages(){
@@ -38,11 +87,3 @@ function hiddenImages(){
     document.getElementById("1").innerHTML.src = img1;
     
 }
-
-
-
-
-// //counts the moves of the user
-// function moves() {
-//     score++;
-// }
